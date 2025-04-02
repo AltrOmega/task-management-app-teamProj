207dac1e58ad4e3d8c1bbb0ed78057f9943c0df6
AltrOmega <michal.szalachowski.33@gmail.com>
2025-04-02 10:43:40 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="sidebar">
+        <div class=option-bar>
+            <div class="sorting">
+                <select>
+                    <option class="option" value="data_desc" selected>Od najnowszego</option>
+                    <option class="option" value="data_asc">Od ostatniego</option>
+                </select>
+            </div>
+            <div class="search-bar">
+                <input type="text" placeholder="WYSZUKAJ...">
+            </div>
+        </div>
+        <div class="tasks">
+            <div class="task">ZADANIE 1</div>
+            <div class="task">ZADANIE 2</div>
+            <div class="task">ZADANIE 3</div>
+            <div class="task completed">ZADANIE 4 ✓</div>
+            <div class="task">ZADANIE 1</div>
+            <div class="task">ZADANIE 2</div>
+            <div class="task">ZADANIE 3</div>
+            <div class="task completed">ZADANIE 4 ✓</div>
+            <div class="task">ZADANIE 1</div>
+            <div class="task">ZADANIE 2</div>
+            <div class="task">ZADANIE 3</div>
+            <div class="task completed">ZADANIE 4 ✓</div>
+            <div class="task">ZADANIE 1</div>
+            <div class="task">ZADANIE 2</div>
+            <div class="task">ZADANIE 3</div>
+            <div class="task completed">ZADANIE 4 ✓</div>
+            <div class="task">ZADANIE 1</div>
+            <div class="task">ZADANIE 2</div>
+            <div class="task">ZADANIE 3</div>
+            <div class="task completed">ZADANIE 4 ✓</div>
+        </div>
+        <a href="/add_page" class="add-button">DODAJ ZADANIE</a>
+    </div>
+    <div class="content">
+        <p>W tym etapie studenci stworzą szczegółowy prototyp aplikacji w narzędziu do prototypowania...</p>
+    </div>
+</div>
